0ccb8802f330f82e98b73e23204cb4b242aa622c
User01 <27apbj@mail.plx>
2024-07-04 10:24:33 +0300

    Добавьте файлы проекта.

---
+++ kvalic/Pages/ListProduct.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kvalic.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ListProduct.xaml
+    /// </summary>
+    public partial class ListProduct : Page
+    {
+        public ListProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text == "Введите для поиска")
+            {
+                textBox.Text = "";
+                textBox.Foreground = Brushes.Black;
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "Введите для поиска";
+                textBox.Foreground = Brushes.Gray;
+            }
+        }
+    }
+}
